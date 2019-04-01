7c8ab2fb1ca687b4d902a698a893d4ae3ad431ae
samanthajowilson <36824168+samanthajowilson@users.noreply.github.com>
2019-04-01 07:03:17 -0500

    Update MainActivity.java
    this change published the email intent and removed the orderSummary method

--- MainActivity.java
+++ MainActivity.java
@@ -9,6 +9,8 @@
 package com.example.samanthawilson.justjava;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -32,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-   public void submitOrder(View view) {
+    public void submitOrder(View view) {
         CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
         boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
 
@@ -45,8 +47,18 @@ public class MainActivity extends AppCompatActivity {
 
         int price = calculatePrice(hasWhippedCream, hasChocolate);
         String priceMessage = createOrderSummary(price, hasWhippedCream, hasChocolate, name);
-        displayMessage(priceMessage);
-   }
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java Order For: " + name);
+        intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+//        displayMessage(priceMessage);
+    }
+//   }
+
 
     private int calculatePrice(boolean hasWhippedCream, boolean hasChocolate) {
         int basePrice = 5;
@@ -56,11 +68,12 @@ public class MainActivity extends AppCompatActivity {
         if (hasChocolate) {
             basePrice = basePrice + 2;
         }
-    if (quantity <= 0) {
+        if (quantity <= 0) {
             basePrice = 0;
-    }
+        }
         return quantity * basePrice;
     }
+
     private String createOrderSummary(int price, boolean addWhippedCream, boolean addChocolate, String nameEditText) {
         String priceMessage = "Name: " + nameEditText;
         priceMessage += "\nAdd whipped cream? " + addWhippedCream;
@@ -71,24 +84,32 @@ public class MainActivity extends AppCompatActivity {
         return priceMessage;
     }
 
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
-    }
+//    /**
+//     * This method displays the given text on the screen.
+//     */
+//    private void displayMessage(String message) {
+//        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+//        priceTextView.setText(message);
+//    }
+
     /**
      * This method is called when the plus button is clicked.
      */
     public void increment(View view) {
+        if (quantity >= 100) {
+            return;
+        }
         quantity = (quantity + 1);
         display(quantity);
     }
+
     /**
      * This method is called when the minus button is clicked.
      */
     public void decrement(View view) {
+        if (quantity == 1) {
+            return;
+        }
         quantity = (quantity - 1);
         display(quantity);
     }
@@ -100,12 +121,13 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
-    /**
-     *This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
+
+//    /**
+//     * This method displays the given price on the screen.
+//     */
+//    private void displayPrice(int number) {
+//        TextView priceTextView = findViewById(R.id.price_text_view);
+//        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+//    }
 
 }
